185c2a9abfeecb175987de2c59b690d6bdcb6e50
Bikram Ghuku <bikramghuku05@gmail.com>
2024-10-02 08:03:55 +0530

    feat: added request maker

--- main.go
+++ main.go
@@ -1,17 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
+type NoticeElement struct {
+	SlNo           string `json:"slno"`
+	EventType      string `json:"event_type"`
+	MessageSubject string `json:"message_subject"`
+	MessageBody    string `json:"message_body"`
+	ApprovedOn     string `json:"approved_on"`
+	Attachment     int64  `json:"primary_attachemnt_id"`
+}
+
+var (
+	ERPJSession        string
+	ERPSSOToken        string
+	NoticeEndpoint     string
+	FileEndpoint       string
+	erpCatCodeTopicMap map[int]string
+)
+
+func MakeRequest(channel int) *http.Request {
+	client, err := http.NewRequest("GET", fmt.Sprintf(NoticeEndpoint, channel), nil)
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+
+	client.AddCookie(&http.Cookie{
+		Name:  "JSESSION",
+		Value: ERPJSession,
+	})
+
+	client.AddCookie(&http.Cookie{
+		Name:  "ssoToken",
+		Value: ERPSSOToken,
+	})
+	return client
+}
+
 func main() {
 	godotenv.Load()
+	ERPJSession = os.Getenv("JSESSIONID")
+	ERPSSOToken = os.Getenv("ssoToken")
 
-	erpCatCodeTopicMap := map[int]string{
+	erpCatCodeTopicMap = map[int]string{
 		11:   "Academic",
 		12:   "Administrative",
 		13:   "Miscellaneous",
@@ -19,14 +59,21 @@ func main() {
 		1002: "Academic (PG) section notices",
 	}
 
-	endpoint := "https://erp.iitkgp.ac.in/InfoCellDetails/internal_noticeboard/get_notice_list.htm?cat_code=%d"
+	NoticeEndpoint = "https://erp.iitkgp.ac.in/InfoCellDetails/internal_noticeboard/get_notice_list.htm?cat_code=%d"
+	FileEndpoint = "https://erp.iitkgp.ac.in/InfoCellDetails/resources/external/groupemailfile?file_id=%s"
 
-	resp, err := http.Get(fmt.Sprintf(endpoint, ""))
-
+	client := &http.Client{}
+	req := MakeRequest(12)
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Printf(err.Error())
+		log.Println(err.Error())
 	}
 
-	fmt.Println(resp.StatusCode)
+	var resBody []NoticeElement
 
+	if err := json.NewDecoder(resp.Body).Decode(&resBody); err != nil {
+		log.Println(err.Error())
+	}
+
+	fmt.Printf("%v", resBody[0])
 }
